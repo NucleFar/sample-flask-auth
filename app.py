6f6d989f6a7845a096269dfca0c256eb42699be7
Alex Alex Desatoff <alexdesatoff2007@gmail.com>
2025-02-12 03:19:26 -0300

    Implementing user roles

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from models.user import User
 from database import db
 from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+import bcrypt
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -27,7 +28,7 @@ def login():
         # Login
         user = User.query.filter_by(username=username).first()
 
-        if user and user.password == password:
+        if user and bcrypt.checkpw(str.encode(password), str.encode(user.password)):
             login_user(user)
             print(current_user.is_authenticated)
             return jsonify({"message": "Autenticacao realizada com sucesso"})
@@ -38,7 +39,7 @@ def login():
 @login_required
 def logout():
     logout_user()
-    return jsonify({'message': "Logout realizado com sucesso!"})
+    return jsonify({"message": "Logout realizado com sucesso!"})
 
 @app.route('/user', methods=['POST'])
 def create_user():
@@ -47,7 +48,8 @@ def create_user():
     password = data.get("password")
 
     if username and password:
-        user = User(username=username, password=password)
+        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt())
+        user = User(username=username, password=hashed_password, role='user')
         db.session.add(user)
         db.session.commit()
         return jsonify({"message": "Usuário cadastrado com sucesso!"})
@@ -70,6 +72,9 @@ def update_user(id_user):
     data = request.json
     user = User.query.get(id_user)
 
+    if id_user != current_user.id and current_user.role == 'user':
+        return jsonify({'message': "Operação não permitida!"}), 403
+
     if user and data.get("password"):
         user.password = data.get("password")
         db.session.commit()
@@ -83,6 +88,9 @@ def update_user(id_user):
 def delete_user(id_user):
     user = User.query.get(id_user)
 
+    if current_user.role != 'admin':
+        return jsonify({"message": "Operação não permitida!"}), 403
+    
     if id_user == current_user.id:
         return jsonify({"message": "Ação não permitida (Usuário logado)!"}), 403
 
